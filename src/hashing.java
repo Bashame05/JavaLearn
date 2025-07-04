340d916c89de2898dee0573c10743d6412ddc5cc
Bashame05 <makizenin1601@gmail.com>
2025-07-04 11:29:04 +0530

    HashSets and HashMaps

--- src/hashing.java
+++ src/hashing.java
@@ -1,31 +1,58 @@
-import java.util.HashMap;
-import java.util.Map;
+//import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+//import java.util.Map;
 
 public class hashing {
     public static void main(String[] args){
-        //creating a HashMap
-        HashMap<String,String> hashira = new HashMap<>();
+//        //creating a HashMap
+//        HashMap<String,String> hashira = new HashMap<>();
+//
+//        //insertion into a hashmap
+//        hashira.put("Shinobu","Insect Hashira");
+//        hashira.put("Sanemi","Wind Hashira");
+//        hashira.put("Mitsuri","Love Hashira");
+//        hashira.put("Giyu","Water Hashira");
+//
+//        //searching in a hashmap
+//        System.out.println(hashira.get("Shinobu"));
+//        System.out.println(hashira.containsKey("Mitsuri"));
+//        System.out.println(hashira.containsValue("Insect Hashira"));
+//
+//        //iterating through a hashmap
+//        //for(Map.entry<keytype,valuetype> variable : hashmapName.entrySet())
+//        for(Map.Entry<String,String> e : hashira.entrySet()){
+//            System.out.print("["+e.getKey()+":"+e.getValue()+"] ");
+//        }
+//        System.out.println();
+//        //removing a key:value pair
+//        hashira.remove("Sanemi");
+//        System.out.print(hashira);
 
-        //insertion into a hashmap
-        hashira.put("Shinobu","Insect Hashira");
-        hashira.put("Sanemi","Wind Hashira");
-        hashira.put("Mitsuri","Love Hashira");
-        hashira.put("Giyu","Water Hashira");
+        //creating a hashSet
+        HashSet<String> yonkos = new HashSet<>();
 
-        //searching in a hashmap
-        System.out.println(hashira.get("Shinobu"));
-        System.out.println(hashira.containsKey("Mitsuri"));
-        System.out.println(hashira.containsValue("Insect Hashira"));
+        //insertion
+        yonkos.add("Luffy");
+        yonkos.add("Shanks");
+        yonkos.add("WhiteBeard");
+        yonkos.add("Kaido");
+        yonkos.add("BlackBeard");
 
-        //iterating through a hashmap
-        //for(Map.entry<keytype,valuetype> variable : hashmapName.entrySet())
-        for(Map.Entry<String,String> e : hashira.entrySet()){
-            System.out.print("["+e.getKey()+":"+e.getValue()+"] ");
+        //printing
+        System.out.println(yonkos);
+
+        //searching
+        System.out.println(yonkos.contains("Shanks"));
+
+        //deletion
+        yonkos.remove("BlackBeard");
+        System.out.println(yonkos);
+
+        //iteration through a set
+        Iterator<String> it = yonkos.iterator();
+        while(it.hasNext()){
+            System.out.print(it.next()+" ");
         }
-        System.out.println();
-        //removing a key:value pair
-        hashira.remove("Sanemi");
-        System.out.print(hashira);
-
     }
 }
